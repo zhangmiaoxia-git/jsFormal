ed66359eb2afb91bc5ad09489133347858db017f
zhangmiaoxia-git <837422796@qq.com>
2020-01-07 09:30:26 +0800

    2020-1-6

---
+++ 2019-1-6/app/src(router)/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <router-link to="/" tag="button">首页</router-link>
+      <router-link to="/about" tag="button">关于</router-link>
+      <router-link to="/public" tag="button">公共页</router-link>
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
